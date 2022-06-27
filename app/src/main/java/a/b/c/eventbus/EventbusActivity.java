9411988d2bab2341fbae1a3b82f68c17e9da3745
hejinwang <hejinwang2022@letsdot.com>
2022-06-27 15:15:52 +0800

    添加Gson和eventbus框架

---
+++ app/src/main/java/a/b/c/eventbus/EventbusActivity.java
@@ -0,0 +1,14 @@
+package a.b.c.eventbus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventbusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventbus);
+    }
+}
